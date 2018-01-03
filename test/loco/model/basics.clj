6c940e363c071be1e35148c8264e9bca19ee09f3
Paul Iannazzo <somethingitalian@gmail.com>
2018-01-03 17:03:26 -0500

    add a test to model.basics from core-tests.clj

--- test/loco/model/basics.clj
+++ test/loco/model/basics.clj
@@ -7,6 +7,32 @@
 (deftest basics-test
   (is
    (=
+    [[:var :x :public [:int 1 5]]
+     [:var :y :public [:int 1 5]]
+     [:var :z :public [:int 1 5]]
+     [:var :2 :hidden [:const 2]]
+     [:constraint [:all-equal [:z :2]]]
+     [:constraint [:arithm [:x :< :y]]]
+     [:constraint [:arithm [:y :<= :z]]]
+     [:constraint [:arithm [:y :> :x]]]
+     [:constraint [:arithm [:z :>= :y]]]
+     [:constraint [:not-all-equal [:x :y]]]
+     [:constraint [:not-all-equal [:x :y :z]]]]
+    (->>
+     [($in :x 1 5)
+      ($in :y 1 5)
+      ($in :z 1 5)
+      ($= :z 2)
+      ($< :x :y)
+      ($<= :y :z)
+      ($> :y :x)
+      ($>= :z :y)
+      ($!= :x :y)
+      ($!= :x :y :z)]
+     model/compile)))
+
+  (is
+   (=
     [[:var :i :public [:int 0 5]]]
     (->> [($in :i 0 5)]
          model/compile)))
