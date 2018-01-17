474b70c3048db7265d1a6cfd26ec159b286ec764
Paul Iannazzo <somethingitalian@gmail.com>
2018-01-17 15:51:33 -0500

    comment out choco.clj (isn't used, doesn't compile)

--- src/loco/choco.clj
+++ src/loco/choco.clj
@@ -1,4 +1,4 @@
-(ns loco.choco
+#_(ns loco.choco
   loco.core :as core :refer [->choco ->choco* *solver*]
   (:import (org.chocosolver.solver.constraints
             Arithmetic
@@ -12,20 +12,14 @@
 
            org.chocosolver.solver.constraints.nary.automata.FA.FiniteAutomaton))
 
-;; arithmetic
+;; arithmetic modifiers
 
-(defn- $+view
+#_(defn- $+view
   [x const]
   (let [model (:csolver *solver*)]
     (.intOffsetView model x const)))
 
-(defn- $*view
+#_(defn- $*view
   [x const]
   (let [model (:csolver *solver*)]
     (.intScaleView model x const)))
-
-(defmethod ->choco* :regular
-  [{:keys [list-of-vars automaton]}]
-  (let [list-of-vars (map ->choco-int-var list-of-vars)
-        model (:csolver *solver*)]
-    (.regular model (into-array IntVar list-of-vars) automaton)))
