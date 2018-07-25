fb7292b01af6af0358623e4b4e50aa114e35ce7e
Paul Iannazzo <somethingitalian@gmail.com>
2018-07-25 12:49:45 -0400

    Fix $- constraint-fn to be more like $+
    Fix up $- tests

--- src/loco/constraints/arithmetic/subtraction.clj
+++ src/loco/constraints/arithmetic/subtraction.clj
@@ -1,9 +1,11 @@
 (ns loco.constraints.arithmetic.subtraction
   (:require
-   [loco.constraints.utils :refer :all]
    [clojure.core.match :refer [match]]
    [loco.constraints :refer [$arithm $sum]]
+   [loco.constraints.utils :refer :all]
    [loco.constraints.views.minus :refer [$minus]]
+   [loco.constraints.views.offset :refer [$offset]]
+   [loco.utils :refer [p c split]]
    ))
 
 (def ^:private partial-name '-)
@@ -13,25 +15,37 @@
          [partial-name body]
          (apply str (interpose (name partial-name) body))))
 
-(defn- constraint-fn [var-name [op [operand1 & more]]]
-  (if (empty? more)
-    [($arithm var-name = ($minus operand1))]
-    (let [negative-vars (->> more (mapv $minus))]
-      [($sum var-name = (into [operand1] negative-vars))])))
+(defn- constraint-fn [& partial]
+  (let [[var-name [op body]] partial]
+    (let [[operand1 rest-operands] ((juxt first rest) body)
+          negative-vars (->> rest-operands (mapv #(if (int? %) (- %) ($minus %))))
+          [numbers vars] (split int? rest-operands)
+          return (match
+                  [operand1 vars numbers]
+                  [nil [] []] nil
+                  [(only-arg :guard int?) [] []] (- only-arg)
+                  [only-arg [] []] ($minus only-arg)
+                  [(only-arg :guard int?) [] nums] (- only-arg (apply + nums))
+                  [op1 [] nums] ($offset op1 (- (apply + nums)))
+                  [_ _ _] ($sum var-name '= (apply vector operand1 negative-vars))
+                  )]
+      [return]
+      )
+    ))
 
 (defn- domain-fn [partial]
   (match partial
          [partial-name body]
          (->
-          (reduce
-           (fn [{:keys [lb ub] :as acc} domain]
-             (match domain
-                    ;;TODO: handle enumerated domains
-                    {:int true :lb cur-lb :ub cur-ub}
-                    (do
-                      {:lb (unchecked-subtract (int lb) (int cur-ub))
-                       :ub (unchecked-subtract (int ub) (int cur-lb))})))
-           body)
+          (->> body
+               (map domainize)
+               (reduce
+                     (fn [{:keys [lb ub] :as acc} domain]
+                       (match domain
+                              ;;TODO: handle enumerated domains
+                              {:int true :lb cur-lb :ub cur-ub} {:lb (- lb cur-ub)
+                                                                 :ub (- ub cur-lb)}))
+                     ))
           (assoc :int true))))
 
 (defloco $-
@@ -41,10 +55,10 @@
   ($= :eq ($- :n1 :n2 :n3 4)) => ($sum :eq := :n1 -:n2 -:n3 -4)
   "
   {:partial true}
-
-  ([& args]
-   {:pre [(not-empty args)]}
-   (if (= 1 (count args))
-     ($minus (first args))
-     (partial-constraint
-      partial-name (vec args) name-fn constraint-fn domain-fn))))
+  [& args]
+  (partial-constraint
+   partial-name
+   (vec args)
+   ;;name-fn
+   :constraint-fn constraint-fn
+   :domain-fn domain-fn))
